5058f7c7fa72f55be1ab8faa41ceb0768b8f6a3c
hopeVaughn <hopevaughnwarren@gmail.com>
2023-04-19 16:22:43 -0700

    add index for handlers

---
+++ src/handlers/index.ts
@@ -0,0 +1,17 @@
+import { createNewUser, signin, } from "./user";
+import { addAlert, getAllAlerts, getAlertById, acknowledgeAlert } from "./alerts"
+import { addCamera, getAllCameras, getCameraById, updateCamera, deleteCamera } from './camera'
+
+export {
+  createNewUser,
+  signin,
+  addAlert,
+  getAllAlerts,
+  getAlertById,
+  acknowledgeAlert,
+  addCamera,
+  getAllCameras,
+  getCameraById,
+  updateCamera,
+  deleteCamera,
+}
