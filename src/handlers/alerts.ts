5aa520379426df0805cba559e3b0368e0fc60197
hopeVaughn <hopevaughnwarren@gmail.com>
2023-04-19 16:15:16 -0700

    add boiler for handlers

---
+++ src/handlers/alerts.ts
@@ -0,0 +1,17 @@
+import { RequestHandler } from 'express';
+import prisma from '../db';
+
+
+export const addAlert: RequestHandler = async (req, res) => {
+  //
+}
+export const getAllAlerts: RequestHandler = async (req, res) => {
+  //
+}
+export const getAlertById: RequestHandler = async (req, res) => {
+  //
+}
+
+export const acknowledgeAlert: RequestHandler = async (req, res) => {
+  //
+}
---
+++ src/handlers/camera.ts
@@ -0,0 +1,18 @@
+import { RequestHandler } from 'express';
+import prisma from '../db';
+
+export const addCamera: RequestHandler = async (req, res) => {
+  //
+}
+export const getAllCameras: RequestHandler = async (req, res) => {
+  //
+}
+export const getCameraById: RequestHandler = async (req, res) => {
+  //
+}
+export const updateCamera: RequestHandler = async (req, res) => {
+  //
+}
+export const deleteCamera: RequestHandler = async (req, res) => {
+  //
+}
