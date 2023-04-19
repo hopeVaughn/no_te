9407666a43ff6d0a7d9d81221fd2b3f365312fac
hopeVaughn <hopevaughnwarren@gmail.com>
2023-04-19 13:48:02 -0700

    update dependencies and auth file

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -1,47 +1,70 @@
-import jwt from 'jsonwebtoken'
-import bcrypt from 'bcrypt'
+import jwt, { Secret } from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import { Request, RequestHandler } from 'express';
 
-export const comparePasswords = (password, hash) => {
-  return bcrypt.compare(password, hash)
+interface User {
+  id: string;
+  name: string;
 }
 
-export const hashPassword = (password) => {
-  return bcrypt.hash(password, 5)
+interface AuthenticatedRequest extends Request {
+  user: User;
 }
+// Compare the given plaintext password with the stored hashed password
+export const comparePasswords = (password: string, hash: string): Promise<boolean> => {
+  return bcrypt.compare(password, hash);
+};
 
-export const createJWT = (user) => {
-  const token = jwt.sign({
-    id: user.id,
-    username: user.name
-  },
-    process.env.JWT_SECRET)
-  return token
-}
+// Hash the given plaintext password using bcrypt with a salt round of 5
+export const hashPassword = (password: string): Promise<string> => {
+  return bcrypt.hash(password, 5);
+};
 
-export const protect = (req, res, next) => {
-  const bearer = req.headers.authorization
+// Create a JWT token for the user, including user ID and username as the payload
+export const createJWT = (user: User): string => {
+  const token = jwt.sign(
+    {
+      id: user.id,
+      username: user.name,
+    },
+    process.env.JWT_SECRET as Secret
+  );
+  return token;
+};
 
+// Middleware function to protect routes that require authentication
+export const protect: RequestHandler = (req: AuthenticatedRequest, res, next) => {
+  // Get the authorization header from the request
+  const bearer = req.headers.authorization;
+
+  // If the authorization header is not present, return an unauthorized error
   if (!bearer) {
-    res.status(401)
-    res.json({ message: 'not authorized' })
-    return
+    res.status(401);
+    res.json({ message: 'not authorized' });
+    return;
   }
 
-  const [, token] = bearer.split(' ')
+  // Extract the token from the authorization header
+  const [, token] = bearer.split(' ');
+
+  // If the token is not present, return an unauthorized error
   if (!token) {
-    res.status(401)
-    res.json({ message: 'not authorized' })
-    return
-  }
-  try {
-    const user = jwt.verify(process.env.JWT_SECRET)
-    req.user = user
-    next()
-  } catch (e) {
-    console.error(e)
-    res.status(401)
-    res.json({ message: 'not valid token' })
-    return
+    res.status(401);
+    res.json({ message: 'not authorized' });
+    return;
   }
 
-}
+  try {
+    // Verify the token using the secret key and add the decoded user to the request object
+    const user = jwt.verify(token, process.env.JWT_SECRET as Secret) as User;
+    req.user = user;
+    // Proceed to the next middleware or route handler
+    next();
+  } catch (e) {
+    // If an error occurs during token verification, return an unauthorized error
+    console.error(e);
+    res.status(401);
+    res.json({ message: 'not valid token' });
+    return;
+  }
+};
