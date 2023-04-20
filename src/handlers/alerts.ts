efb62c15dd72eadad379ac6f2a872f1ba1df8b3c
hopeVaughn <hopevaughnwarren@gmail.com>
2023-04-19 18:06:23 -0700

    update error handler

--- src/handlers/alerts.ts
+++ src/handlers/alerts.ts
@@ -99,7 +99,7 @@ export const getAllAlerts: RequestHandler = async (req: AuthenticatedRequest, re
 
     res.status(200).json(alerts);
   } catch (error) {
-    console.error(error);
+    console.error(error.message);
     res.status(500).json({ message: 'Error fetching alerts' });
   }
 };
