1daa4808b2dbb841013a532b83c895018e7a422d
hopeVaughn <hopevaughnwarren@gmail.com>
2023-04-20 13:06:55 -0700

    update testing

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleNameMapper: {
+    "@exmpl/(.*)": "<rootDir>/src/$1"
+  },
 };
---
+++ src/__tests__/handlers.test.ts
@@ -0,0 +1,5 @@
+import request from "supertest"
+
+describe("test create route", () => {
+  // add tests here
+})
