f5415629edaae5b7ed9eb20523a602d36d57e1af
hopeVaughn <hopevaughnwarren@gmail.com>
2023-04-20 11:41:57 -0700

    update package.json and add seed data for camera's

---
+++ prisma/seed.ts
@@ -0,0 +1,28 @@
+import { PrismaClient, CameraStatus } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // Seed 12 unique cameras
+  for (let i = 1; i <= 12; i++) {
+    await prisma.camera.create({
+      data: {
+        name: `Camera ${i}`,
+        location: `Location ${i}`,
+        status: i % 2 === 0 ? CameraStatus.ONLINE : CameraStatus.OFFLINE,
+        videoUrl: `http://example.com/video${i}`,
+      },
+    });
+  }
+
+  console.log("Seeded 12 cameras");
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
--- src/handlers/camera.ts
+++ src/handlers/camera.ts
@@ -1,18 +1,118 @@
+// src/handlers/camera.ts
 import { RequestHandler } from 'express';
 import prisma from '../db';
+import { AuthenticatedRequest } from '../modules/auth';
 
-export const addCamera: RequestHandler = async (req, res) => {
-  //
-}
+// Request handler to add a new camera
+export const addCamera: RequestHandler = async (req: AuthenticatedRequest, res) => {
+  const { name, location, status, videoUrl } = req.body;
+
+  // Validate the request data
+  if (!name || !location || !status || !videoUrl) {
+    res.status(400);
+    res.json({ message: 'Invalid request data' });
+    return;
+  }
+
+  // Create a new camera record in the database
+  const newCamera = await prisma.camera.create({
+    data: {
+      name,
+      location,
+      status,
+      videoUrl,
+    },
+  });
+
+  // Return the created camera record
+  res.status(201);
+  res.json(newCamera);
+};
+
+// Request handler to fetch all cameras
 export const getAllCameras: RequestHandler = async (req, res) => {
-  //
-}
+  try {
+    const cameras = await prisma.camera.findMany();
+    res.status(200).json(cameras);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ message: 'Error fetching cameras' });
+  }
+};
+
+// Request handler to fetch a camera by its ID
 export const getCameraById: RequestHandler = async (req, res) => {
-  //
-}
-export const updateCamera: RequestHandler = async (req, res) => {
-  //
-}
-export const deleteCamera: RequestHandler = async (req, res) => {
-  //
-}
+  const id = req.params.id;
+
+  // Validate the request data
+  if (!id) {
+    res.status(400);
+    res.json({ message: 'Invalid request data' });
+    return;
+  }
+
+  // Find the camera by its ID
+  const camera = await prisma.camera.findUnique({
+    where: { id },
+  });
+
+  // If the camera is not found, return a 404 error
+  if (!camera) {
+    res.status(404);
+    res.json({ message: 'Camera not found' });
+    return;
+  }
+
+  // Return the found camera
+  res.status(200);
+  res.json(camera);
+};
+
+// Request handler to update a camera by its ID
+export const updateCamera: RequestHandler = async (req: AuthenticatedRequest, res) => {
+  const id = req.params.id;
+  const { name, location, status, videoUrl } = req.body;
+
+  // Validate the request data
+  if (!id || !name || !location || !status || !videoUrl) {
+    res.status(400);
+    res.json({ message: 'Invalid request data' });
+    return;
+  }
+
+  // Update the camera record in the database
+  const updatedCamera = await prisma.camera.update({
+    where: { id },
+    data: {
+      name,
+      location,
+      status,
+      videoUrl,
+    },
+  });
+
+  // Return the updated camera record
+  res.status(200);
+  res.json(updatedCamera);
+};
+
+// Request handler to delete a camera by its ID
+export const deleteCamera: RequestHandler = async (req: AuthenticatedRequest, res) => {
+  const id = req.params.id;
+
+  // Validate the request data
+  if (!id) {
+    res.status(400);
+    res.json({ message: 'Invalid request data' });
+    return;
+  }
+
+  // Delete the camera record from the database
+  const deletedCamera = await prisma.camera.delete({
+    where: { id },
+  });
+
+  // Return the deleted camera record
+  res.status(200);
+  res.json(deletedCamera);
+};
