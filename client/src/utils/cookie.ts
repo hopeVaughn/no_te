95859e5bc84c22c4f2d9f7315f257cb55b77268a
hopeVaughn <hopevaughnwarren@gmail.com>
2023-04-24 22:26:23 -0700

    add optioncahing to cookies.ts

--- client/src/utils/cookie.ts
+++ client/src/utils/cookie.ts
@@ -12,7 +12,7 @@ export function getCookie(name: string) {
   const value = '; ' + document.cookie;
   const parts = value.split('; ' + name + '=');
   if (parts.length === 2) {
-    return parts.pop().split(';').shift();
+    return parts.pop()?.split(';').shift();
   }
   return null;
 }
