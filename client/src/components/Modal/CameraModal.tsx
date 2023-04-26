77433c4b69fc0c2cf560e51f9fe4eda08ed0c887
hopeVaughn <hopevaughnwarren@gmail.com>
2023-04-26 11:35:05 -0700

    update evenet listener in modal

--- client/src/components/Modal/CameraModal.tsx
+++ client/src/components/Modal/CameraModal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 interface CameraModalProps {
   videoUrl: string;
@@ -6,6 +6,8 @@ interface CameraModalProps {
 }
 
 const CameraModal: React.FC<CameraModalProps> = ({ videoUrl, onClose }) => {
+  const outerDivRef = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
       if (event.key === 'Escape') {
@@ -13,21 +15,35 @@ const CameraModal: React.FC<CameraModalProps> = ({ videoUrl, onClose }) => {
       }
     };
 
+    const handleClickOutside = (event: MouseEvent) => {
+      if (event.target === outerDivRef.current) {
+        onClose();
+      }
+    };
+
     window.addEventListener('keydown', handleKeyDown);
+    if (outerDivRef.current) {
+      outerDivRef.current.addEventListener('click', handleClickOutside);
+    }
 
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
+      if (outerDivRef.current) {
+        outerDivRef.current.removeEventListener('click', handleClickOutside);
+      }
     };
   }, [onClose]);
 
   return (
-    <div className="fixed top-0 left-0 w-full h-full z-10 flex justify-center items-center bg-black bg-opacity-50">
+    <div
+      ref={outerDivRef}
+      className="fixed top-0 left-0 w-full h-full z-10 flex justify-center items-center bg-black bg-opacity-50"
+    >
       <div className="w-full h-full max-w-4xl max-h-screen bg-white rounded-xl p-4 flex justify-center items-center relative">
         <iframe
-          className="w-full h-full"
+          className="w-full h-full border-none"
           src={videoUrl}
-          title="YouTube video player"
-          frameBorder="0"
+          title="Video player"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
         ></iframe>
