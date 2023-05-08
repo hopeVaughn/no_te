37d0f499a78571d8ec5bf6815472136a0042c0a4
hopeVaughn <hopevaughnwarren@gmail.com>
2023-05-08 09:16:34 -0700

    remove white space

--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,32 @@
 import express, { Request, Response, NextFunction } from 'express';
-import router from './router'
-import morgan from 'morgan'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
-import { protect } from './handlers/modules/auth'
-import { createNewUser, signin } from './handlers/user'
+import router from './router';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import { protect } from './handlers/modules/auth';
+import { createNewUser, signin } from './handlers/user';
 const app = express();
 
 //--MiddleWare
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(cors({
   origin: 'http://localhost:5173',
   credentials: true,
 }));
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 //--End of MiddleWare
 
 app.get('/', (req, res) => {
   console.log('hello from express');
-  res.status(200)
-  res.json({ message: "hello" })
-})
+  res.status(200);
+  res.json({ message: "hello" });
+});
 
-app.use('/api', protect, router)
-app.post('/user', createNewUser)
-app.post('/signin', signin)
+app.use('/api', protect, router);
+app.post('/user', createNewUser);
+app.post('/signin', signin);
 
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   if (err.type === 'auth') {
@@ -38,5 +38,4 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   }
 });
 
-
-export default app
+export default app;
